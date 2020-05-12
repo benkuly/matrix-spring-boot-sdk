6c500044b35942f230277795cb0e31488f261362
benkuly <benkuly@users.noreply.github.com>
2020-05-12 13:18:00 +0200

    removed defaultvalue annotations (not needed in kotlin)

--- matrix-spring-boot-rest-appservice/src/main/kotlin/net/folivo/matrix/appservice/config/MatrixAppserviceProperties.kt
+++ matrix-spring-boot-rest-appservice/src/main/kotlin/net/folivo/matrix/appservice/config/MatrixAppserviceProperties.kt
@@ -2,7 +2,6 @@ package net.folivo.matrix.appservice.config
 
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.context.properties.ConstructorBinding
-import org.springframework.boot.context.properties.bind.DefaultValue
 
 @ConfigurationProperties("matrix.appservice")
 @ConstructorBinding
@@ -12,16 +11,12 @@ data class MatrixAppserviceProperties(
         val namespaces: Namespaces = Namespaces()
 ) {
     data class Namespaces(
-            @DefaultValue("[]")
             val users: List<Namespace> = emptyList(),
-            @DefaultValue("[]")
             val aliases: List<Namespace> = emptyList(),
-            @DefaultValue("[]")
             val rooms: List<Namespace> = emptyList()
     )
 
     data class Namespace(
-            @DefaultValue("true")
             val exclusive: Boolean = true,
             val regex: String
     )
--- matrix-spring-boot-rest-client/src/main/kotlin/net/folivo/matrix/restclient/config/MatrixClientProperties.kt
+++ matrix-spring-boot-rest-client/src/main/kotlin/net/folivo/matrix/restclient/config/MatrixClientProperties.kt
@@ -2,7 +2,6 @@ package net.folivo.matrix.restclient.config
 
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.context.properties.ConstructorBinding
-import org.springframework.boot.context.properties.bind.DefaultValue
 
 @ConfigurationProperties("matrix.client")
 @ConstructorBinding
@@ -12,9 +11,7 @@ data class MatrixClientProperties(
 ) {
     data class MatrixHomeServerProperties(
             val hostname: String,
-            @DefaultValue("443")
-            val port: Int,
-            @DefaultValue("true")
-            val secure: Boolean
+            val port: Int = 443,
+            val secure: Boolean = true
     )
 }
