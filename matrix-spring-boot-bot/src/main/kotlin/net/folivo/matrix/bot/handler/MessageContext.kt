34b6e716b897b83644988233293f33345b3666eb
benkuly <benkuly@users.noreply.github.com>
2020-06-08 09:56:44 +0200

    better handling of not working answers too room

--- matrix-spring-boot-bot/src/main/kotlin/net/folivo/matrix/bot/handler/MessageContext.kt
+++ matrix-spring-boot-bot/src/main/kotlin/net/folivo/matrix/bot/handler/MessageContext.kt
@@ -2,13 +2,20 @@ package net.folivo.matrix.bot.handler
 
 import net.folivo.matrix.core.model.events.m.room.message.MessageEvent
 import net.folivo.matrix.restclient.MatrixClient
+import org.slf4j.LoggerFactory
 import reactor.core.publisher.Mono
+import reactor.util.retry.Retry
+import java.time.Duration
 
 class MessageContext(
         val matrixClient: MatrixClient,
         val originalEvent: MessageEvent<*>,
         val roomId: String
 ) {
+
+    companion object {
+        private val LOG = LoggerFactory.getLogger(this::class.java)
+    }
 
     fun answer(
             content: MessageEvent.MessageEventContent,
@@ -18,7 +25,8 @@ class MessageContext(
                 roomId = roomId,
                 eventContent = content,
                 asUserId = asUserId
-        )
+        ).retryWhen(Retry.backoff(3, Duration.ofMillis(500)))
+                .doOnError { LOG.warn("could not answer to $roomId", it) }
+                .onErrorResume { Mono.empty() }
     }
-
 }
